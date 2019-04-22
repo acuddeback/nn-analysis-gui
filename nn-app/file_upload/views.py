5e9158f00fba8d18263eb779767dc49c8673b0bd
acuddeback <atc@cuddeback.org>
2019-04-22 01:51:33 -0400

    upload a file and saves Rplots.pdf from output of analysis.r script

--- nn-app/file_upload/views.py
+++ nn-app/file_upload/views.py
@@ -1,32 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .forms import UploadFileForm
+from django.urls import reverse
 
 import subprocess
 
 # Create your views here.
 
+def handle_uploaded_file(f):
+    with open('file_upload/app-scripts/input.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+
 def index(request):
     return HttpResponse("Hello, world. You're at the file upload index.")
-
-
 
 def home(request):
     form = UploadFileForm()
     return render(request, 'index.block.html', {'form': form})
 
 def upload_file(request):
-  return HttpResponse("You tried to upload a file.")
-    # if request.method == 'POST':
-    #     form = UploadFileForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         # handle_uploaded_file(request.FILES['file'])
-    #         return HttpResponseRedirect('/success/url/')
-    # else:
-    #     form = UploadFileForm()
-    # return render(request, 'index.block.html', {'form': form})
+  if request.method == 'POST':
+      form = UploadFileForm(request.POST, request.FILES)
+      print(request.POST)
+      print(form.is_valid())
+      if(form.is_valid()):
+          handle_uploaded_file(request.FILES['file'])
+          return HttpResponseRedirect('/runscript/')
+  else:
+      form = UploadFileForm()
+  return HttpResponseRedirect(reverse('home'))
   
 def runscript(request):
   form = UploadFileForm()
-  subprocess.call (["/usr/bin/Rscript --vanilla file_upload/app-scripts/analysis.r file_upload/app-scripts/example.txt"], shell=True)
+  subprocess.call (["/usr/bin/Rscript --vanilla file_upload/app-scripts/analysis.r file_upload/app-scripts/input.txt"], shell=True)
   return render(request, 'index.block.html', {'form': form})
