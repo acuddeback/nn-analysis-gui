2538a7eb8a747be9336eb497fe5ffc1c37c21a54
acuddeback <atc@cuddeback.org>
2019-04-29 23:04:41 -0400

    update results in view.py

--- nn-app/file_upload/views.py
+++ nn-app/file_upload/views.py
@@ -35,6 +35,8 @@ def upload_file(request):
 def runscript(request):
   form = UploadFileForm()
   subprocess.call (["/usr/bin/Rscript --vanilla file_upload/app-scripts/analysis.r file_upload/app-scripts/input.txt"], shell=True)
+  open('file.txt', 'w').close()
   return render(request, 'results.block.html')
 
 def results(request):
+  return render(request, 'results.block.html')
