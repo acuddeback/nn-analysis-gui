d39ea6ba60738f5a1b6013a1ea83eee4f2cb55fc
Jacob Blindenbach <43180795+jBlinden@users.noreply.github.com>
2019-05-08 10:52:14 -0400

    Added Input Checking and Error Handling

--- nn-app/file_upload/app-scripts/analysis.r
+++ nn-app/file_upload/app-scripts/analysis.r
@@ -1,6 +1,5 @@
 #install.packages('NeuralNetTools',repos = "http://cran.us.r-project.org")
-#Add this if you need to install neural network tools ^
-
+#Add this if you need to install the neural network tools library ^
 library(NeuralNetTools)
 
 args <- commandArgs(TRUE)
@@ -10,24 +9,32 @@ file <- args[1]
 fileRaw <- readLines(file)
 
 my_weights <- as.double(unlist(strsplit(fileRaw[1],split=",")))
+
+#print(length(my_weights))
+
 my_struct <- as.integer(unlist(strsplit(fileRaw[2],split=",")))
 my_inputs <- unlist(strsplit(fileRaw[3],split=","))
 my_outputs <- unlist(strsplit(fileRaw[4],split=","))
 
-plotnet(my_weights,my_struct,x_names=my_inputs,y_names=my_outputs)
+tryCatch({
+  plotnet(my_weights,my_struct,x_names=my_inputs,y_names=my_outputs)
+},error=function(cond){
+	message("Incorrect number of weights and biases in input")
+})
 
 wts_in <- neuralweights(my_weights, struct = my_struct)
 wts_struct <- wts_in$struct
 wts_in <- wts_in$wts
 
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[1],y_lab=my_outputs[1])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[2],y_lab=my_outputs[2])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[3],y_lab=my_outputs[3])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[4],y_lab=my_outputs[4])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[5],y_lab=my_outputs[5])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[6],y_lab=my_outputs[6])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[7],y_lab=my_outputs[7])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[8],y_lab=my_outputs[8])
-olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[9],y_lab=my_outputs[9])
 
-
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[1],y_lab=my_outputs[1]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[2],y_lab=my_outputs[2]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[3],y_lab=my_outputs[3]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[4],y_lab=my_outputs[4]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[5],y_lab=my_outputs[5]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[6],y_lab=my_outputs[6]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[7],y_lab=my_outputs[7]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[8],y_lab=my_outputs[8]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[9],y_lab=my_outputs[9]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[10],y_lab=my_outputs[10]),silent=TRUE)
+try(olden(wts_in,my_inputs,my_outputs,out_var=my_outputs[11],y_lab=my_outputs[11]),silent=TRUE)
